bc4ac1f079b00c5c4cb0c6a9019e9b3ebd0cf2ed
Matthew  Brabham <matthew.brabham@oregonstate.edu>
2024-12-10 15:29:54 -0800

    Adding a remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -17,3 +17,5 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+-- Autoformat
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
