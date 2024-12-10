9565878b2d30c4d8a9f84f7f5a12cc0fe69ae655
Matthew  Brabham <matthew.brabham@oregonstate.edu>
2024-12-10 15:29:41 -0800

    Adding some fun things

---
+++ lua/config/moar.lua
@@ -0,0 +1,16 @@
+local augroup = vim.api.nvim_create_augroup
+local MoarGroup = augroup('Moar', {})
+local autocmd = vim.api.nvim_create_autocmd
+autocmd('LspAttach', {
+    group = MoarGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
